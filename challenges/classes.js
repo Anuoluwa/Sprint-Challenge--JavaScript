8c2673e59f5aaf2159c2780a3c6ab5bb2df90bbf
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-10 10:50:27 +0100

    completes Sprint challenge stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -13,14 +13,35 @@
       }
   }
 
+  class cubeMaker extends CuboidMakerClass  {
+    constructor(attributes) {
+      super(attributes);
+    }
+    volume () {
+      return (this.width ** 3);
+    }
+    surfaceArea () {
+      return 6 * (this.width ** 2);
+    }
+}
+
+
+
   const cuboidClass = new CuboidMakerClass({
     length: 4,
     width: 5,
     height: 5
   })
 
+  const cube = new cubeMaker({
+    width: 7
+  })
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
+console.log(cube.volume()); // 343 for cube
+console.log(cube.surfaceArea()); // 294 for cube
+
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
